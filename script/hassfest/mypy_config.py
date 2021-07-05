a41731a75d7f3a8e252724b0de5b48f866d8168c
Tobias Sauerwein <cgtobi@gmail.com>
2021-07-05 09:36:37 +0000

    Add type annotations

--- script/hassfest/mypy_config.py
+++ script/hassfest/mypy_config.py
@@ -121,7 +121,6 @@ IGNORED_MODULES: Final[list[str]] = [
     "homeassistant.components.neato.*",
     "homeassistant.components.ness_alarm.*",
     "homeassistant.components.nest.*",
-    "homeassistant.components.netatmo.*",
     "homeassistant.components.netio.*",
     "homeassistant.components.nightscout.*",
     "homeassistant.components.nilu.*",
